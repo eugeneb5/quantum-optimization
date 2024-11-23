d71bab54723b6b2f61dc0aabdc757981974b5811
eugeneb5 <69252905+eugeneb5@users.noreply.github.com>
2024-11-23 16:37:42 +0000

    save point

--- itse.py
+++ itse.py
@@ -85,3 +85,115 @@ H = H_ising(n,M,k)
 
 print(H.shape)
 
+def s(psi, H, t):
+    return -H(t)*psi
+
+def s_time_independent(psi,H):
+
+    return -H*psi
+
+
+def RK4(dim, total_time, n,psi_0,H, is_time_dependent):  #n is number of iterations
+
+    dt = total_time/n
+
+    #ts = np.arrange(total_time)*dt
+
+    t = 0
+
+    ys = np.zeros((n,2**dim))
+
+    y = psi_0
+
+    def dpsi_dt (psi, H, t):
+
+        if is_time_dependent == True:
+
+            return -1*np.dot(H(t),psi)  #this is for imaginary time!!
+        else:
+
+            return -1*np.dot(H,psi)
+        
+
+
+    for i in range(n):
+
+        ys[i] = y
+
+        
+
+        k_0 = dt*(dpsi_dt(y,H,t))
+        k_1 = dt*(dpsi_dt(y+(k_0/2), H, t+(dt/2)))
+        k_2 = dt*(dpsi_dt(y+(k_1/2), H, t+(dt/2)))
+        k_3 = dt*(dpsi_dt(y+k_2 , H, t+dt))
+
+        y = y + (1/6)*(k_0+2*k_1+2*k_2+k_3)
+
+        y = y/np.linalg.norm(y)  #normalize psi at each time step
+
+        t += dt
+
+
+    return ys
+
+
+def psi_initial(n):
+
+    return np.random.uniform(low = 1e-10, high = 1, size = 2**n)  #low is exclusive, high is inclusive
+
+
+
+
+# initialize test for time-independent H:
+H = H_ising(5,M,k)
+
+psi_0 = psi_initial(5)
+
+
+
+
+r = RK4(5,100,1000,psi_0,H,is_time_dependent=False)
+
+print(r.shape)
+
+print(r[999,18])
+
+#print(len(r))
+
+
+#what should i graph?
+# try E_res first!
+
+def graph_E_res(H_classic, values, known_eigenvalue):  #requires knowledge of what eigenvalue is! for n=5 case, min value is -6.5
+
+
+    n = len(values)
+
+    y_val = np.zeroes(n)
+
+    for i in range(n):
+
+        psi = values[i]
+
+        E_actual = np.dot(psi.conj().T, np.dot(H_classic,psi))
+
+        y_val[i] = E_actual - known_eigenvalue
+
+        
+
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
